e77d42337aacacaf47a39efd6f8835db4349c3fc
Freeman Jiang <freeman.jiang.ca@gmail.com>
2022-04-22 05:26:53 -0400

    Add selective lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  "**/*.{ts,tsx}": (filenames) => ["yarn format:fix", "yarn validate"],
+  // Type check TypeScript files
+  "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
+
+  // Lint then format TypeScript and JavaScript files
+  "**/*.(ts|tsx|js)": (filenames) => [
+    `yarn eslint ${filenames.join(" ")}`,
+    `yarn prettier --write ${filenames.join(" ")}`,
+  ],
+
+  // Format MarkDown and JSON
+  "**/*.(md|json)": (filenames) =>
+    `yarn prettier --write ${filenames.join(" ")}`,
 };
